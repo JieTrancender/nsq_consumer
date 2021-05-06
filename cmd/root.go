d24012aef794470a1ba00fd6c45e9ac5502d7845
JTrancender <jie-email@jie-trancender.org>
2021-05-06 22:24:55 +0800

    feat: using go-svc to manage the project (#4)
    * feat: using go-svc to manage the project
    
    * fix: lint wrong

--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,78 @@
 package cmd
 
 import (
+	"os"
+	"os/signal"
+	"path/filepath"
+	"sync"
+	"syscall"
+
 	"github.com/JieTrancender/nsq_to_consumer/internal/lg"
+	"github.com/JieTrancender/nsq_to_consumer/nsq_consumer"
+
+	"github.com/judwhite/go-svc"
 
 	"github.com/spf13/cobra"
 )
+
+type program struct {
+	once     sync.Once
+	consumer *nsq_consumer.NsqConsumer
+}
 
 var rootCmd = &cobra.Command{
 	Use:   "kbm",
 	Short: "kbm means keyboard man service.",
 	Run: func(cmd *cobra.Command, args []string) {
-		logInfo("%s", "Hello World!")
+		prg := &program{}
+		if err := svc.Run(prg, syscall.SIGINT); err != nil {
+			logFatal("%s", err)
+		}
 	},
+}
+
+func (p *program) Init(env svc.Environment) error {
+	if env.IsWindowsService() {
+		dir := filepath.Dir(os.Args[0])
+		return os.Chdir(dir)
+	}
+	return nil
+}
+
+func (p *program) Start() error {
+	opts := nsq_consumer.NewOptions()
+	consumer, err := nsq_consumer.NewNsqConsumer(opts)
+	if err != nil {
+		logFatal("new nsq consumer fail, err: %s", err)
+	}
+	p.consumer = consumer
+
+	signalChan := make(chan os.Signal, 1)
+	go func() {
+		for range signalChan {
+			p.once.Do(func() {
+				p.consumer.Exit()
+			})
+		}
+	}()
+	signal.Notify(signalChan, syscall.SIGTERM)
+
+	go func() {
+		err := p.consumer.Main()
+		if err != nil {
+			_ = p.Stop()
+			os.Exit(1)
+		}
+	}()
+
+	return nil
+}
+
+func (p *program) Stop() error {
+	p.once.Do(func() {
+		p.consumer.Exit()
+	})
+	return nil
 }
 
 func Execute() {
@@ -22,8 +83,4 @@ func Execute() {
 
 func logFatal(f string, args ...interface{}) {
 	lg.LogFatal("[nsq_consumer] ", f, args...)
-}
-
-func logInfo(f string, args ...interface{}) {
-	lg.LogInfo("[nsq_consumer] ", f, args...)
 }
