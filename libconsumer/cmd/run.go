6c2e13d22c1bb4752f390cf4c5b1db1c4d9fd140
Jason <jie-email@jie-trancender.org>
2021-08-05 17:01:05 +0800

    feat: add nsqd consumer

--- libconsumer/cmd/run.go
+++ libconsumer/cmd/run.go
@@ -7,6 +7,7 @@ import (
 	"github.com/JieTrancender/nsq_to_consumer/internal/version"
 	"github.com/JieTrancender/nsq_to_consumer/libconsumer/cmd/instance"
 	"github.com/JieTrancender/nsq_to_consumer/libconsumer/consumer"
+	"github.com/JieTrancender/nsq_to_consumer/libconsumer/logp"
 	"github.com/spf13/cobra"
 )
 
@@ -24,7 +25,7 @@ func genRunCmd(settings instance.Settings, ct consumer.Creator) *cobra.Command {
 
 			err := instance.Run(settings, ct)
 			if err != nil {
-				fmt.Println(err)
+				logp.L().Errorf("instance Run fail:%v", err)
 				os.Exit(1)
 			}
 		},
